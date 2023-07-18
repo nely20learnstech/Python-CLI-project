36db225a0ae30afd63b1595aed37d2627efd2a7c
NELY Zaratiana Fify <99418437+nely20learnstech@users.noreply.github.com>
2023-07-18 15:59:11 +0300

    Update model.py

--- First-Needed Good Stock Management/model.py
+++ First-Needed Good Stock Management/model.py
@@ -1,29 +1,26 @@
 import datetime
 
-class Expirable_grocery:
-    def __init__(self, name, category, quantity, product_creation_date, expiration_date,
-                 created_at=None, updated_at = None, id=None):
+class Grocery:
+    def __init__(self, name, category, quantity, created_at=None, updated_at=None, id=None):
         self.name = name
         self.category = category
         self.quantity = quantity
+        self.created_at = created_at if created_at is not None else datetime.datetime.now()
+        self.updated_at = updated_at if updated_at is not None else datetime.datetime.now()
+        self.id = id if id is not None else None
+
+
+class Expirable_grocery(Grocery):
+    def __init__(self, name, category, quantity, product_creation_date, expiration_date,
+                 created_at=None, updated_at=None, id=None):
+        super().__init__(name, category, quantity, created_at, updated_at, id)
         self.product_creation_date = product_creation_date
         self.expiration_date = expiration_date
-        self.created_at = created_at if created_at is not None else datetime.datetime.now()
-        self.updated_at = updated_at if updated_at is not None else datetime.datetime.now()
-        self.id = id if id is not None else None
 
 
-class Unexpirable_grocery:
-    def __init__(self, name, category, quantity, 
-                 created_at=None, updated_at=None, id=None
-                ):
-        self.name = name
-        self.category = category
-        self.created_at = created_at if created_at is not None else datetime.datetime.now()
-        # self.updated_at = datetime.datetime.now() if updated_at is not None else datetime.datetime.now()
-        self.updated_at = updated_at if updated_at is not None else datetime.datetime.now()
-        self.quantity = quantity
-        self.id = id if id is not None else None
+class Unexpirable_grocery(Grocery):
+    def __init__(self, name, category, quantity, created_at=None, updated_at=None, id=None):
+        super().__init__(name, category, quantity, created_at, updated_at, id)
 
 
 class Minimal_information:
